fae577e1d2697d9eb7e7c3658ec164c84f35d9cf
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-05 15:33:23 +0900

    Add part of chapter 7, error reporting to mail and log file

--- flask/microblog/config.py
+++ flask/microblog/config.py
@@ -1,9 +1,21 @@
 import os
+
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "aisndof23894ucnefmcx"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(base_dir, 'app.db')
+
+    # DB setup for SQL alchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(base_dir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Mail setup for error handling
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["examples@abc.com"]  # Address that will receive error
