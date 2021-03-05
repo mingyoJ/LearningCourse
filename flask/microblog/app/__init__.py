cb1c0b0c2cee980201a94119b3d3761d204970ce
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-05 14:53:45 +0900

    Add part of chapter 7, error handling pages

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -8,11 +8,11 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = "login"
 
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+from app import routes, models, errors
