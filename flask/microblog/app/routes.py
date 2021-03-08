fe2e94ca92fbc64a5592bff6446053a7a45b16ae
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-08 10:46:11 +0900

    [Flask Chatper 9] Add explore and href of user at post

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -38,10 +38,7 @@ def index():
         flash("Your post is now live!")
         return redirect(url_for("index"))
 
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
+    posts = current_user.followed_posts().all()
     return render_template("index.html", title="Home Page", form=form, posts=posts)
 
 
@@ -176,3 +173,10 @@ def unfollow(username):
         return redirect(url_for("user", username=username))
     else:
         return redirect(url_for("index"))
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template("index.html", title="Explore", posts=posts)
