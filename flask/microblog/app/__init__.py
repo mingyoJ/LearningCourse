4311e99a4af8173d3f41d81a593d47576c97ca6a
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-08 18:58:25 +0900

    [Flask Chapter 11] Add bootstrap to prettify microblog

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -20,6 +21,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     # Send debugging error message to mail
