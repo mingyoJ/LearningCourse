f209b6aa81ead19d2a952c8601b72065b25fa60d
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-08 11:24:32 +0900

    [Flask Chatper 9] Add newer/older button for page navigation at posts

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -42,8 +42,16 @@ def index():
     posts = current_user.followed_posts().paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
+
+    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
     return render_template(
-        "index.html", title="Home Page", form=form, posts=posts.items
+        "index.html",
+        title="Home Page",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
     )
 
 
@@ -99,13 +107,32 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"},
-    ]
+
+    page = request.args.get("page", 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+    next_url = (
+        url_for("user", username=user.username, page=posts.next_num)
+        if posts.has_next
+        else None
+    )
+    prev_url = (
+        url_for("user", username=user.username, page=posts.prev_num)
+        if posts.has_prev
+        else None
+    )
+
     form = EmptyForm()
 
-    return render_template("user.html", user=user, posts=posts, form=form)
+    return render_template(
+        "user.html",
+        user=user,
+        posts=posts.items,
+        form=form,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.before_request
@@ -187,4 +214,14 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
-    return render_template("index.html", title="Explore", posts=posts.items)
+
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
