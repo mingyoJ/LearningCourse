533b88cc0015cc19454cdeb881c85d25b556e0bc
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-09 12:32:05 +0900

    [Flask Chapter 12] Add moment module for time

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -23,6 +24,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 if not app.debug:
     # Send debugging error message to mail
