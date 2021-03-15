2d646a177f748cd5e32d3a570da4c7a09957a4a0
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-15 15:45:22 +0900

    [Flask Chapter 21] Add view about message (send, browse)

--- flask/microblog/app/models.py
+++ flask/microblog/app/models.py
@@ -86,6 +86,19 @@ class User(UserMixin, db.Model):
         backref=db.backref("followers", lazy="dynamic"),
         lazy="dynamic",
     )
+    messages_sent = db.relationship(
+        "Message",
+        foreign_keys="Message.sender_id",
+        backref="author",
+        lazy="dynamic",
+    )
+    messages_received = db.relationship(
+        "Message",
+        foreign_keys="Message.recipient_id",
+        backref="recipient",
+        lazy="dynamic",
+    )
+    last_message_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -140,6 +153,14 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return (
+            Message.query.filter_by(recipient=self)
+            .filter(Message.timestamp > last_read_time)
+            .count()
+        )
+
 
 @login.user_loader
 def load_user(id):
@@ -156,3 +177,14 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    recipient_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Message {self.body}>"
