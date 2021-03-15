aea4496d05042906d6dddf6c36509ce425169d45
MinGyo Jung <mingyo.jung@vuno.co>
2021-03-15 18:08:07 +0900

    [Flask Chapter 21] Add notification and its model for realtime msg update

--- flask/microblog/app/models.py
+++ flask/microblog/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from hashlib import md5
+import json
 from time import time
 
 from flask import current_app
@@ -99,6 +100,7 @@ class User(UserMixin, db.Model):
         lazy="dynamic",
     )
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship("Notification", backref="user", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -161,6 +163,12 @@ class User(UserMixin, db.Model):
             .count()
         )
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -188,3 +196,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return f"<Message {self.body}>"
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
